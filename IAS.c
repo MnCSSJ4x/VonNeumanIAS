1c7ac143df947e18cedac7af7eeae96303ea3b25
MnCSSJ4x <Monjot.Choudhury@iiitb.ac.in>
2021-04-25 18:47:14 +0530

    Added Some decode Instruction. Changed MBR struct . Now has DATA,left,right.

--- IAS.c
+++ IAS.c
@@ -1,10 +1,10 @@
-#include <IAS.h>
+#include "IAS.h"
 
 void fetch(int value_at_pc)
 {
     MAR = value_at_pc;
-    MBR = MainMemory[MAR]; //data_
-
+    MBR.lhs = MainMemory[MAR] >> 20; //data_
+    MBR.rhs = MainMemory[MAR] & 0b0000000000000000000011111111111111111111;
     IBR = MBR.rhs;
     IR = (MBR.lhs) >> 12;
     MAR = ((MBR.lhs)) & 0b00000000111111111111;
@@ -12,7 +12,7 @@ void fetch(int value_at_pc)
 
     if (left_flag == 0)
     {
-        decode(IR, MAR);
+        decode_execute(IR, MAR);
         //decode on right
     }
     else
@@ -22,12 +22,93 @@ void fetch(int value_at_pc)
     }
 }
 
-void decode(int IR, int MAR)
+void decode_execute(int IR, int MAR)
 {
-    switch (IR)
+    switch (IR) //Takes opcode
     {
-        //load-> abs , normal , -
-    }   //stor
-        //artih + - * as + division
-}       //factorial
-        //Jumps + conditional jumps
+        //MEMORY BASED
+    case 0b00001010:
+        printf("Detected LoadMQ\n");
+        ACC = MQ;
+        printf("Transfer of MQ to ACC Done.\n"); //Load MQ
+        break;
+    case 0b00001001:
+        MBR.data = MainMemory[MAR];
+        MQ = MBR.data;
+        //load MQ M(X)
+        break;
+    case 0b00100001: //Stor M(X)
+        printf("Detected STOR M(X)");
+        ACC = MBR.data;
+        MainMemory[MAR] = MBR.data;
+        printf("Stored Succesfully");
+        break;
+    case 0b00000001: //Load M(X)
+        printf("Detected LOAD M(X)");
+        MBR.data = MainMemory[MAR];
+        ACC = MBR.data;
+        printf("Load successful\n");
+        break;
+
+    case 0b00000010:
+        printf("Detected -LOAD M(X)\n");
+        MBR.data = MainMemory[MAR];
+        ACC = -MBR.data;
+        printf("Load successful\n");
+        break;
+    case 0b00000100:
+        printf("Detected LOAD -| M(X)|\n");
+        MBR.data = -(long long int)fabs(MainMemory[MAR]);
+        ACC = MBR.data;
+        printf("Load successful\n");
+        break;
+    case 0b00000011:
+        printf("Detected LOAD| M(X) |");
+        MBR.data = MainMemory[MAR];
+        ACC = (long long int)fabs(MBR.data);
+        printf("Load successful\n");
+        break;
+
+        // ARITHMATIC BASED
+
+    case 0b00000101:
+        printf("Detected add M(X)\n");
+        MBR.data = MainMemory[MAR];
+        ACC += MBR.data;
+        printf("Added to AC succesfully\n");
+        break;
+    case 0b00000111:
+        printf("Detected add |M(X)|\n");
+        MBR.data = (long long int)fabs(MainMemory[MAR]);
+        ACC += MBR.data;
+        printf("Added to AC succesfully\n");
+        break;
+    case 0b00000110:
+        printf("Detected sub M(X)\n");
+        MBR.data = MainMemory[MAR];
+        ACC -= MBR.data;
+        printf("Subtracted from AC succesfully\n");
+        break;
+    case 0b00001000:
+        printf("Detected sub |M(X)|\n");
+        MBR.data = (long long int)fabs(MainMemory[MAR]);
+        ACC -= MBR.data;
+        printf("Subtracted from AC succesfully\n");
+        break;
+    case 0b00010100:
+        printf("Detected Left Shift\n");
+        ACC = ACC << 1;
+        printf("Left shift successful\n");
+        break;
+    case 0b00010101:
+        printf("Detected Right Shift\n");
+        ACC = ACC >> 1;
+        printf("Right shift successful\n");
+        break;
+
+        //JUMP BASED
+    }
+}
+void PrintStatus()
+{
+}
--- IAS.h
+++ IAS.h
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<math.h>
+#include <math.h>
 #define MAX_SIZE 1000
 #define SIZE_PC 10
-
+#define BUFFER 500
 //Instruction format
 
 typedef struct instruction
 {
-  int lhs;
-  int rhs;
+  long long int lhs;
+  long long int rhs;
+  long long int data;
 } instruction;
 
 //Registers
 
-instruction MainMemory[MAX_SIZE];          
+long long int MainMemory[MAX_SIZE];
 int PC[SIZE_PC];
 int MAR;
 int IR;
@@ -23,4 +24,4 @@ instruction MBR;
 int ACC;
 int MQ;
 int PC_index = 0;
-int left_flag = 0;          //If we have a single instruction _> by default its right instruction so 0 
+int left_flag = 0; //If we have a single instruction _> by default its right instruction so 0
